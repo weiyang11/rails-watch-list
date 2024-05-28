27f2878c1e0fd8e0cc2f212c6d1d95ef96455c61
weiyang <weiyang_11@outlook.com>
2024-05-28 21:36:15 +0800

    Add dotenv-rails

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'up' => 'rails/health#show', as: :rails_health_check
 
   root to: 'lists#index'
-  resources :lists, only: [:index, :new, :show, :create] do
+  resources :lists, only: [:new, :show, :create] do
     resources :bookmarks, only: [:new, :create]
   end
   resources :bookmarks, only: [:destroy]
